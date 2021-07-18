585baeb2fc9efbd359df0197cc67d96438d1f3b3
Syed MH <hassan@syedmh.com>
2021-07-18 23:00:34 +0500

    Added a dynamic routing structure that i think will work

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
       :image="featured.featuredImage.formats.large.url"
     />
     <home-about :title="about.designation" :content="about.content" />
-    <lazy-home-recent-projects :projects="recentProjects" />
+    <home-recent-projects :projects="recentProjects" />
   </main>
 </template>
 
@@ -23,13 +23,13 @@ export default {
   },
   async fetch() {
     const _featured = await fetch(
-      'https://strapi.syedmh.com/projects?highlight=true&_limit=1'
+      `${process.env.API_BASE_URL}/projects?highlight=true&_limit=1`
     ).then((_response) => _response.json())
     this.about = await fetch('https://strapi.syedmh.com/home-about').then(
       (_response) => _response.json()
     )
     const _recentProjects = await fetch(
-      'https://strapi.syedmh.com/projects?highlight=false'
+      `${process.env.API_BASE_URL}/projects?highlight=false&_sort=published_at:DESC&_limit=3`
     ).then((_response) => _response.json())
     this.featured = _featured[0]
     this.recentProjects = _recentProjects.slice(0, 3)
