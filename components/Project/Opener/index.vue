63457d66b7bc1d42668878b83b29faa1707176b5
Syed MH <hassan@syedmh.com>
2021-07-20 00:11:55 +0500

    adjusted srcset code to see if it works now

--- components/Project/Opener/index.vue
+++ components/Project/Opener/index.vue
@@ -2,8 +2,7 @@
   <div>
     <figure aspect-ratio="8x3" class="rounded shadow filter">
       <img
-        :srcset="`${$config.API_BASE_URL}/${image.small.url} 330w, ${$config.API_BASE_URL}/${image.medium.url} 500w`"
-        sizes="330px, 500px"
+        :srcset="`${$config.API_BASE_URL}/${image.thumbnail.url} ${image.thumbnail.width}w, ${$config.API_BASE_URL}/${image.small.url} ${image.small.width}w, ${$config.API_BASE_URL}/${image.medium.url} ${image.medium.width}w`"
         :src="`${$config.API_BASE_URL}/${image.large.url}`"
         :alt="alt"
       />
